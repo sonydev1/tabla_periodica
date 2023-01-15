6991f9fd2deab837540b338f45385e2717e80af8
Victor DEV <sonydev404@gmail.com>
2023-01-14 19:27:57 -0500

    mejorando el diseño

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -5,21 +5,19 @@ function Element({ elemen }) {
     return (
       <div
         style={{ background: elemen.color, color: elemen.textColor }}
-        className="  md:w-[61.78px] md:h-[80px] xl:w-[90px] xl:h-[90px]  rounded-md"
+        className="w-[61.78px] h-[82px]  md:w-[61.78px] md:h-[82px] xl:w-[90px] xl:h-[92px]  rounded-md"
         key={elemen.id}
       >
-        <button className="w-full  px-2 ">
+        <button className="w-full px-2">
           <div className="text-end">
             <div>
-              <p className="text-[12px] xl:text-md ">{elemen.id}</p>
+              <p className="text-[12px] xl:text-md font-bold">{elemen.id}</p>
             </div>
           </div>
           <div className="text-left">
-            <p className="text-3xl   font-semibold">
-              {elemen.simbolo}
-            </p>
-              <p className="text-[8.7px] ">{elemen.n_atomico}</p>
-            <p className=" text-[9px] mt-1 w-9 break-normal">{elemen.name}</p>
+            <p className="text-3xl font-semibold -mt-2">{elemen.simbolo}</p>
+            <p className="text-[9px] ">{elemen.n_atomico}</p>
+            <p className=" text-[10px]  break-all">{elemen.name}</p>
           </div>
         </button>
       </div>
